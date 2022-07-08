78102085235c4eedc2dcb06471eafa834cdc07f5
Yarooq Anwar <yarooq1@gmail.com>
2022-07-08 23:48:21 +0500

    hope

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -21,19 +21,34 @@ function Review() {
         // console.log(data)
         setReview(data);
 
-        setReview((data) => {
-            console.log(data);
-            return data;
-        })
+        // setReview((data) => {
+        //     console.log(data);
+        //     return data;
+        // })
         // console.log(review)
 
         // notUseState = data.data
     }
 
     return (
-        <div>
-            {/* <h2>{notUseState.attributes.title}</h2> */}
+<div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">Reviews</h2>
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              <a href="#" className="group">
+                {/* <div className="max-h-36 aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                  <img
+                    src={product.attributes.reviewimg.data.attributes.name}
+                    alt={product.attributes.title}
+                    className="object-center object-cover group-hover:opacity-75"
+                  />
+                </div> */}
+                <h3 className="mt-4 text-sm text-gray-700">{review.attributes.title}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">{review.attributes.review}</p>
+              </a>          
         </div>
+      </div>
+    </div>
 
     )
 }
