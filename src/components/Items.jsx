1d12460e7db98340b75dec2f1cd58874a59a37c1
Yarooq Anwar <yarooq1@gmail.com>
2022-07-13 23:05:31 +0500

    new

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import {BrowserRouter asLink} from "react-router-dom"
+import {Link} from "react-router-dom"
 
 export default function Items() {
   const [products, setProducts] = useState([])
