c146c7aa6e523685f2af1de8a4d200315d745277
Yarooq Anwar <yarooq1@gmail.com>
2022-07-20 20:52:19 +0500

    new

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -35,12 +35,12 @@ function Review() {
         <section className="text-gray-400 body-font overflow-hidden">
         <div className="container px-5 py-24 mx-auto">
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            {/* <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={refReview.current.reviewimg.data.attributes.name}/> */}
+            <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={refReview.current.reviewimg.data.attributes.name}/>
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h1 className="text-white text-3xl title-font font-medium mb-1">{refReview.current.title}</h1>
               <div className="flex mb-4">
                 <span className="flex items-center">
-                  <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidh="2" className="w-4 h-4 text-pink-400" viewBox="0 0 24 24">
+                  <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidh="2" className="w-4 h-4 text-white" viewBox="0 0 24 24">
                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                   </svg>
                   {/* <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidh="2" className="w-4 h-4 text-red-400" viewBox="0 0 24 24">
@@ -58,7 +58,7 @@ function Review() {
                   {/* <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidh="2" className="w-4 h-4 text-red-400" viewBox="0 0 24 24">
                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                   </svg> */}
-                  <span className="ml-3">{refReview.current.rating + " Star Ratings"}</span>
+                  <span className="ml-3 text-white">{refReview.current.rating + " Star Ratings"}</span>
                 </span>
 
                 {/* <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-800 text-gray-500 space-x-2">
