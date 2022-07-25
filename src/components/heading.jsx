14e2399ec6077f93bf69250ef942af362cde4ac6
Yarooq Anwar <yarooq1@gmail.com>
2022-07-25 21:03:17 +0500

    removed junk code

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -1,24 +1,8 @@
 import '../styles/Heading.css'
 import React, {useState, useEffect} from 'react'
-import {useParams} from 'react-router-dom'
+
 
 function Heading() {
-  const { uid } = useParams();
-
-  const [review, setReview] = useState([]);
-  const [loading, setLoading] = useState(true);
-  
-
-  useEffect(() => {
-    // getReview();
-    console.log(review);
-  }, []);
-
-  // const getReview = async () => {
-  //   const response = await fetch(`http://localhost:1337/api/reviews/${uid}?populate=*`);
-  //   const data = await response.json();
-  //   setReview(data.data);
-  // }
 
   return (
     <div>
